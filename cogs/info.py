bea7fb56690c625b35c27178adcf8a2b2dfe1549
Flavien Jobard-Houdusse <flavien.jobard-houdusse@infomaniak.com>
2025-11-28 13:54:35 +0100

    feat: add prices on gameinfo command

--- cogs/info.py
+++ cogs/info.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from utils.game_info import game_info
+from utils.game_info import game_info,game_price
 
 PERSO = 1098342509324800000
 ROOTFARM = 1288930612048166962
@@ -18,21 +18,33 @@ class Info(commands.Cog):
 
     @app_commands.command(name="gameinfo", description="Avoir la description d'un jeu")
     @app_commands.guilds(discord.Object(id=PERSO),discord.Object(id=ROOTFARM))
-    async def gameinfo(self, interaction: discord.Interaction, jeu: str):
-        jeu=game_info(jeu)
+    async def gameinfo(self, interaction: discord.Interaction, game: str):
+        jeu = game_info(game)
         boxart_url = jeu["assets"].get("boxart")
-        embed = discord.Embed(
+        embed_info = discord.Embed(
                 title = jeu["title"],
-                color = discord.Color.red()
+                color = discord.Color.green()
                 )
         if boxart_url:
-            embed.set_image(url=boxart_url)
+            embed_info.set_image(url=boxart_url)
         else:
-            embed.set_footer(text="No boxart available")
-        embed.add_field(name="Developpeurs", value=jeu["developers"][0]["name"])
-        embed.add_field(name="Date de sortie", value=jeu["releaseDate"])
-        embed.add_field(name="URL", value=f"[View on ITAD]({jeu['urls']['game']})")
-        await interaction.response.send_message(embed=embed)
+            embed_info.set_footer(text="No boxart available")
+        embed_info.add_field(name="Developpeurs", value=jeu["developers"][0]["name"])
+        embed_info.add_field(name="Date de sortie", value=jeu["releaseDate"])
+        embed_info.add_field(name="URL", value=f"[View on ITAD]({jeu['urls']['game']})")
+        prix = game_price(game)
+        embed_prix = discord.Embed(
+                title = "Offres",
+                color = discord.Color.green()
+                )
+        for shop in prix:
+            for deal in shop["deals"]:
+                boutique = deal["shop"]["name"]
+                valeur = deal["price"]["amount"]
+                devise = deal["price"]["currency"]
+                url = deal["url"]
+                embed_prix.add_field(name="", value=f"[{boutique} - {valeur} {devise}]({url})")
+        await interaction.response.send_message(embeds=[embed_info, embed_prix])
 
 async def setup(bot):
     await bot.add_cog(Info(bot))
--- utils/game_info.py
+++ utils/game_info.py
@@ -8,10 +8,15 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY") 
 
 def game_id(name):
-    req = requests.get("https://api.isthereanydeal.com/games/lookup/v1",params={"key": API_KEY,"title":name})
+    req = requests.get("https://api.isthereanydeal.com/games/lookup/v1", params={"key": API_KEY, "title": name})
     return req.json()["game"]["id"]
 
+def game_price(name):
+    gid = game_id(name)
+    req = requests.post("https://api.isthereanydeal.com/games/prices/v3", params={"key": API_KEY, "country": "FR"},json=[gid])
+    return req.json()
+
 def game_info(name):
-    gid=game_id(name)
-    req = requests.get("https://api.isthereanydeal.com/games/info/v2",params={"key": API_KEY,"id":gid})
+    gid = game_id(name)
+    req = requests.get("https://api.isthereanydeal.com/games/info/v2", params={"key": API_KEY, "id": gid})
     return req.json()
